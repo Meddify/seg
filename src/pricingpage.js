5c91f2b2922a11897fcdd5a3d4f6449e19cd4387
Celinaarakji <163094383+Celinaarakji@users.noreply.github.com>
2025-11-02 21:54:07 -0500

    image

--- src/pricingpage.js
+++ src/pricingpage.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Pricing page loaded");
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log('Pricing page ready ');
+  const rows = document.querySelectorAll(".table tbody tr");
+
+
+  const planCells = document.querySelectorAll(".plan");
+  planCells.forEach(plan => {
+    plan.setAttribute("title", `Click to learn more about the ${plan.textContent}plan`);
+  });
+
+
+  rows.forEach(row => {
+    row.addEventListener("click", () => {
+     
+      rows.forEach(r => r.classList.remove("selected"));
+      row.classList.add("selected");
+
+      
+      const plan = row.querySelector(".plan").textContent;
+      const desc = row.querySelector(".desc").textContent
+      const price = row.querySelector(".price").textContent;
+
+      alert(`Plan: ${plan}\nDescription: ${desc}\nPrice: ${price}`);                              
+    });
+  });
 });
+
